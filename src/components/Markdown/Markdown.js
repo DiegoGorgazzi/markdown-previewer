0321a5a64858651063c9539e95aa11cb3c2697db
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-03-12 22:10:14 +0100

    Add ids per project specs

--- src/components/Markdown/Markdown.js
+++ src/components/Markdown/Markdown.js
@@ -4,9 +4,13 @@ const markdown = (props) => {
     return (
       <div>
           <h3>INPUT</h3>
-          <textarea type="text" onChange={props.changed}/>
+          <textarea
+            id="editor"
+            type="text"
+            onChange={props.changed}
+            />
           <h3>OUTPUT </h3>
-          <p>{props.preview}</p>
+          <p id="preview">{props.preview}</p>
       </div>
     )
 };
